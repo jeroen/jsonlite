b79d72544111481e1bab1a176e9872af61fdb22b
Jeroen Ooms <jeroenooms@gmail.com>
2013-10-24 18:51:18 -0400

    Add unit test for factors

---
+++ inst/tests/test-toJSON-factor.R
@@ -0,0 +1,7 @@
+context("Factors")
+
+test_that("Encoding Factor Objects", {
+  expect_that(fromJSON(toJSON(iris$Species)), is_identical_to(as.character(iris$Species)));
+  expect_that(fromJSON(toJSON(iris$Species[1])), is_identical_to(as.character(iris$Species[1])));
+  expect_that(fromJSON(toJSON(iris$Species, factor="integer")), equals(structure(unclass(iris$Species), levels=NULL)));
+});
