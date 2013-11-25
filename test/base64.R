77ab1949893b18cbe34fe23068912546526e8582
Jeroen Ooms <jeroenooms@gmail.com>
2013-11-24 20:56:28 -0800

    remove old files

--- test/base64.R
+++
@@ -1,3 +0,0 @@
-#test if raw vectors are serialized properly
-x <- paste(readLines(system.file("DESCRIPTION", package="JSONlite")), collapse="\n")
-identical(x, rawToChar(JSONlite:::base64_decode(JSONlite:::base64_encode(charToRaw(x)))))
--- test/testjson.R
+++
@@ -1,5 +0,0 @@
-library(encode)
-file.exists("test.json");
-isValidJSON("test.json");
-obj <- fromJSON(readLines("test.json"));
-
