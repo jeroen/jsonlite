f882d817a680b2517e050d0e8f3382033bc22252
Jeroen <jeroenooms@gmail.com>
2013-12-09 11:56:33 -0800

    pretty format numbers

--- R/asJSON.numeric.R
+++ R/asJSON.numeric.R
@@ -8,10 +8,13 @@ setMethod("asJSON", "numeric", function(x, collapse = TRUE, digits = 5, na = "st
     }
   }
   
-  # pretty format numbers
-  # this is a bit slow
+  # pretty format numbers, 'drop0trailing' is super slow for some reason
   # tmp <- formatC(x, digits = digits, format = "f", drop0trailing = TRUE)
   
+  # This is faster, does not use scientific notation
+  # tmp <- sub("\\.0+$", "", sprintf(paste0("%.", digits, "f"), x))
+  
+  # This is perhaps a bit more natural?
   # See options(scipen) for if and how it switches to scientific notation
   tmp <- as.character(round(x, digits))
   
