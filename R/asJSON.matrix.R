a72933adb203e8f74da6b3b9a53865cdcbeefef4
Jeroen Ooms <jeroenooms@gmail.com>
2013-12-09 16:54:39 -0800

    fix for nested matrix

--- R/asJSON.matrix.R
+++ R/asJSON.matrix.R
@@ -1,9 +1,15 @@
 # NOTE: opencpu.encode is never upposed to use this function, because it
 # unclasses every object first.  it is included for completeness.
 
-setMethod("asJSON", "matrix", function(x, collapse = TRUE, ...) {
+setMethod("asJSON", "matrix", function(x, collapse = TRUE, na = NULL, oldna = NULL, ...) {
+  
+  # reset na arg when called from data frame
+  if(identical(na, "NA")){
+    na <- oldna;
+  }
+  
   # row based json
-  tmp <- apply(x, 1, asJSON, ...)
+  tmp <- apply(x, 1, asJSON, na = na, ...)
   
   # collapse it
   if (collapse) {
--- inst/tests/test-fromJSON-dataframe.R
+++ inst/tests/test-fromJSON-dataframe.R
@@ -46,7 +46,10 @@ test_that("recover lists in data frames", {
 #note: nested matrix does not perfectly restore
 test_that("nested matrix in data frame", {
   x <- data.frame(foo=1:2)
-  x$bar <- matrix(1:6, 2)
+  x$bar <- matrix(c(1:5, NA), 2)
+  
+  expect_that(validate(toJSON(x)), is_true())
+  
   y <- fromJSON(toJSON(x))
   expect_that(y, is_a("data.frame"))
   expect_that(names(x), equals(names(y)))
