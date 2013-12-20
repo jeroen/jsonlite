94829371faa7212d54295fb3c9bdcf4b8e0a5b5b
Jeroen Ooms <jeroenooms@gmail.com>
2013-12-20 15:26:10 -0800

    adding toJSON for arrays

--- R/asJSON.matrix.R
+++ R/asJSON.matrix.R
@@ -1,7 +1,4 @@
-# NOTE: opencpu.encode is never upposed to use this function, because it
-# unclasses every object first.  it is included for completeness.
-
-setMethod("asJSON", "matrix", function(x, collapse = TRUE, na = NULL, oldna = NULL, ...) {
+setMethod("asJSON", "array", function(x, collapse = TRUE, na = NULL, oldna = NULL, ...) {
   
   # reset na arg when called from data frame
   if(identical(na, "NA")){
