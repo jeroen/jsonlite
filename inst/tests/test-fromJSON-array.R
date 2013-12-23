9baa0a5e1054a91169161479b66863a28b234430
Jeroen <jeroenooms@gmail.com>
2013-12-22 23:24:45 -0800

    Test for column major encoding

--- inst/tests/test-fromJSON-array.R
+++ inst/tests/test-fromJSON-array.R
@@ -1,6 +1,6 @@
 context("fromJSON Array")
 
-test_that("fromJSON Array", {
+test_that("fromJSON Array, row major", {
 
   # test high dimensional arrays
   lapply(2:5, function(n){
@@ -15,5 +15,21 @@ test_that("fromJSON Array", {
     newobject <- fromJSON(toJSON(object));
     expect_that(object, equals(newobject));
   });  
+});
 
+test_that("fromJSON Array, column major", {
+  
+  # test high dimensional arrays
+  lapply(2:5, function(n){
+    object <- array(1:prod(n), dim=1:n)
+    newobject <- fromJSON(toJSON(object, matrix="columnmajor"), columnmajor=TRUE);
+    expect_that(object, equals(newobject));
+  });
+  
+  # adding some flat dimensions
+  lapply(1:5, function(n){
+    object <- array(1:prod(n), dim=c(1:n, 1))
+    newobject <- fromJSON(toJSON(object, matrix="columnmajor"), columnmajor=TRUE);
+    expect_that(object, equals(newobject));
+  });  
 });
