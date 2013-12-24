5e5a381594e6c913fac5916e2cc44110c63f1e87
Jeroen <jeroenooms@gmail.com>
2013-12-23 21:32:05 -0800

    adding note

--- inst/tests/test-libjson-utf8.R
+++ inst/tests/test-libjson-utf8.R
@@ -1,5 +1,10 @@
 context("libjson UTF-8 characters")
 
+# Some notes: JSON defines UTF-8 as the default charset. Therefore all encoders and
+# decoders are required to support UTF-8. JSON also allows for escaped unicode, i.e 
+# \u00F8 however this is mostly for legacy purposes. Using actual UTF-8 characters 
+# is easier and more efficient.
+
 test_that("test that non ascii characters are ok", {
   
   #create random strings
