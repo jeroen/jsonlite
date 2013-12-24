eceba21eb2d4fcb320f746a023608af8d315fb40
Jeroen Ooms <jeroenooms@gmail.com>
2013-12-23 17:16:26 -0800

    adding tests for libjson

---
+++ inst/tests/test-libjson.R
@@ -0,0 +1,21 @@
+context("libjson")
+
+test_that("escaping and parsing of special characters", {
+  
+  #create random strings
+  mychars <- c('a', 'b', " ", '"', "\\", "\t", "\n", "'", "/", "#", "$");
+  createstring <- function(length){
+    paste(mychars[ceiling(runif(length, 0, length(mychars)))], collapse="")
+  }  
+  
+  #generate 1000 random strings
+  for(i in 1:200){
+    x <- createstring(i);
+    expect_that(x, equals(fromJSON(toJSON(x))));
+  }
+  
+  #try some very long strings
+  x <- list(foo=createstring(1e6))
+  expect_that(x, equals(fromJSON(toJSON(x))));
+
+});
