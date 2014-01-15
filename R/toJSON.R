2134537a35285650e90df5608e17c127cec9a787
Jeroen <jeroenooms@gmail.com>
2014-01-15 10:10:40 -0800

    clean up

--- R/toJSON.R
+++ R/toJSON.R
@@ -24,11 +24,6 @@ toJSON <- function(x, dataframe = c("rows", "columns"), matrix = c("rowmajor", "
   # force
   x <- force(x)
   
-  # a check
-  if(is.atomic(x) && identical(class(x)[1], "AsIs")){
-    warning("Calling toJSON on an atomic vector of class AsIs will likely result in invalid JSON.", call.=FALSE)
-  }
-  
   # dispatch
   ans <- asJSON(x, dataframe = dataframe, Date = Date, POSIXt = POSIXt, factor = factor, 
     complex = complex, raw = raw, matrix = matrix, digits = digits, na = na, ...)
