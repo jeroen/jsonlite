a20d7c61ebdfab43fb054465556609e7367f38a7
Jeroen <jeroenooms@gmail.com>
2014-05-30 12:39:13 -0700

    Pretty error message

--- R/fromJSON.R
+++ R/fromJSON.R
@@ -90,7 +90,7 @@ fromJSON <- function(txt, simplifyVector = TRUE, simplifyDataFrame = simplifyVec
   
   # simple check
   if (!grepl("^[ \t\r\n]*(\\{|\\[)", txt)) {
-    stop("String does not seem to be valid JSON: ", substring(txt, 0, 20))
+    stop("String does not seem to be valid JSON: ", gsub("\\s+"," ",substring(txt, 0, 30)))
   }
   
   # parse JSON
