c9a3a6a0492bb38716e235c1d50cfd25a6c10c83
Jeroen <jeroenooms@gmail.com>
2014-07-23 08:32:59 +0200

    Add print method

--- R/print.R
+++ R/print.R
@@ -3,3 +3,17 @@
 print.json <- function(x, ...){
   cat(x)
 }
+
+#' @method print scalar
+#' @export
+print.scalar <- function(x, ...){
+  original <- x;
+  if(is.data.frame(x)){
+    row.names(x) <- "[X]"
+  } else {
+    names(x) <- "[X]"
+  }
+  class(x) <- class(x)[-1]  
+  print(x)
+  invisible(original)
+}
