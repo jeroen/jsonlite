bffb74c989fd1ea39ccda734738111cd6f48140f
Jeroen <jeroenooms@gmail.com>
2014-07-31 00:02:28 +0200

    Add rbind.pages

---
+++ R/rbind.pages.R
@@ -0,0 +1,28 @@
+rbind.pages <- function(dfs){
+  stopifnot(is.list(dfs))
+  stopifnot(all(vapply(dfs, is.data.frame, logical(1))))
+  
+  # Extract data frame column names
+  dfdf <- lapply(dfs, vapply, is.data.frame, logical(1))
+  dfnames <- names(which(unlist(dfdf)))
+  subdfs <- lapply(dfnames, function(colname){
+    combine_pages(lapply(dfs, `[[`, colname))
+  })
+  names(subdfs) <- dfnames
+  
+  # Remove data frame columns
+  dfs <- lapply(dfs, function(df){
+    df[!vapply(df, is.data.frame, logical(1))]
+  })
+  
+  # Bind rows
+  outdf <- plyr::rbind.fill(dfs)
+  
+  # Combine wih sub dataframes
+  out <- c(outdf, subdfs)
+  class(out) <- "data.frame"
+  row.names(out) <- row.names(outdf)
+  
+  #out
+  out  
+}
