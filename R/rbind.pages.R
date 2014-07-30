f8d9ac35b4928d3f61cfab4162180d1d74c6beff
Jeroen <jeroenooms@gmail.com>
2014-07-31 00:33:25 +0200

    Fixes

--- R/rbind.pages.R
+++ R/rbind.pages.R
@@ -5,8 +5,15 @@ rbind.pages <- function(dfs){
   # Extract data frame column names
   dfdf <- lapply(dfs, vapply, is.data.frame, logical(1))
   dfnames <- unique(names(which(unlist(dfdf))))
+  
+  # No sub data frames
+  if(!length(dfnames)){
+    return(plyr::rbind.fill(dfs))
+  }
+  
+  # Extract the nested data frames
   subdfs <- lapply(dfnames, function(colname){
-    combine_pages(lapply(dfs, `[[`, colname))
+    rbind.pages(lapply(dfs, `[[`, colname))
   })
   
   # Remove data frame columns
