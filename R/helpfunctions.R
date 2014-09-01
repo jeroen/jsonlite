f3aa5081b6936a7b4289bd362cb2c39adfb359ac
Jeroen <jeroenooms@gmail.com>
2014-09-01 19:36:09 +0200

    Change to manual escaping of character vectors

--- R/helpfunctions.R
+++ R/helpfunctions.R
@@ -14,14 +14,26 @@ givename <- function(obj) {
 }
 
 # vectorized deparse
-deparse_vector <- function(x) {
+deparse_vector_old <- function(x) {
   stopifnot(is.character(x))
-
-  #For these characters deparse() generates invalid JSON escape sequences
-  #Compare ?Quotes and http://json.org/
   x <- gsub("[\v\a]", "", x)
   vapply(x, deparse, character(1), USE.NAMES=FALSE)
 }
+
+# this version is ugly but much faster
+deparse_vector <- function(x) {
+  stopifnot(is.character(x))
+  if(!length(x)) return(x)
+  x <- gsub("\\", "\\\\", x, fixed=TRUE)
+  x <- gsub("\"", "\\\"", x, fixed=TRUE)
+  x <- gsub("\n", "\\n", x, fixed=TRUE)
+  x <- gsub("\r", "\\r", x, fixed=TRUE)
+  x <- gsub("\t", "\\t", x, fixed=TRUE)
+  x <- gsub("\b", "\\b", x, fixed=TRUE)
+  x <- gsub("\f", "\\f", x, fixed=TRUE)
+  paste0("\"", x, "\"")
+}
+
 
 # trim whitespace
 trim <- function(x) {
