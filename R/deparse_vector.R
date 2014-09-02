d67dc7d163d78a0fd498f70ed9f4b6f4543db110
Jeroen Ooms <jeroenooms@gmail.com>
2014-09-02 14:29:05 +0200

    Rename function

--- R/deparse_vector.R
+++ R/deparse_vector.R
@@ -3,7 +3,7 @@ deparse_vector_c <- function(x) {
   .Call("C_escape_chars", PACKAGE = "jsonlite", x)
 }
 
-deparse_vector <- function(x) {
+deparse_vector_r <- function(x) {
   stopifnot(is.character(x))
   if(!length(x)) return(x)
   x <- gsub("\\", "\\\\", x, fixed=TRUE)
@@ -15,6 +15,9 @@ deparse_vector <- function(x) {
   x <- gsub("\f", "\\f", x, fixed=TRUE)
   paste0("\"", x, "\"")
 }
+
+# Which implementation to use
+deparse_vector <- deparse_vector_r
 
 #Below are older implementations of the same function
 deparse_vector_old <- function(x) {
