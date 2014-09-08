1bcf938d38f11a0eb34309856ec43b97d95b4b80
Jeroen Ooms <jeroenooms@gmail.com>
2014-09-09 00:07:41 +0200

    Decouple null_to_na from unlist

--- R/list_to_vec.R
+++ R/list_to_vec.R
@@ -1,31 +1,3 @@
 list_to_vec <- function(x) {
-  if (!length(x)) {
-    return(vector())
-  }
-
-  #Start parsing missing values
-  x2 <- x
-  looks_like_character_vector = FALSE
-  for(i in seq_along(x2)){
-    if(is.character(x2[[i]])){
-      x2[[i]] <- switch(x2[[i]],
-        "NA" = NA,
-        "NaN" = NaN,
-        "Inf" = Inf,
-        "-Inf" = -Inf,
-        {looks_like_character_vector=TRUE; break}
-      )
-    }
-  }
-
-  # Set x
-  if(!looks_like_character_vector){
-    x <- x2
-  }
-
-  # Convert NULL to NA
-  x[vapply(x, is.null, logical(1))] <- NA
-
-  # Unlist only if set
-  return(unlist(x, recursive = FALSE, use.names = FALSE))
+  return(unlist(null_to_na(x), recursive = FALSE, use.names = FALSE))
 }
---
+++ R/null_to_na.R
@@ -0,0 +1,31 @@
+null_to_na <- function(x){
+  if (!length(x)) {
+    return(vector())
+  }
+
+  #Start parsing missing values
+  x2 <- x
+  looks_like_character_vector = FALSE
+  for(i in seq_along(x2)){
+    if(is.character(x2[[i]])){
+      x2[[i]] <- switch(x2[[i]],
+        "NA" = NA,
+        "NaN" = NaN,
+        "Inf" = Inf,
+        "-Inf" = -Inf,
+        {looks_like_character_vector=TRUE; break}
+      )
+    }
+  }
+
+  # Set x
+  if(!looks_like_character_vector){
+    x <- x2
+  }
+
+  # Convert NULL to NA
+  x[vapply(x, is.null, logical(1))] <- NA
+
+  #return
+  return(x)
+}
