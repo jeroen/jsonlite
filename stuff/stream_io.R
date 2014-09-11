b0788911810b77e4c960c0e776d8fc896065e0fc
Jeroen Ooms <jeroenooms@gmail.com>
2014-09-11 23:58:14 +0200

    change to gzcon

--- stuff/stream_io.R
+++ stuff/stream_io.R
@@ -1,9 +1,10 @@
 library(jsonlite)
 library(dplyr)
-con_in <- url("http://jeroenooms.github.io/data/diamonds.mjson")
+con_in <- gzcon(url("http://jeroenooms.github.io/data/hflights.mjson.gz"))
 con_out <- file(tmp <- tempfile(), open = "w")
 stream_in(con_in, handler = function(df){
-  df <- dplyr::filter(df, carat > 1)
+  df <- dplyr::filter(df, Distance > 1000)
   stream_out(df, con_out, 1000)
 }, pagesize = 5000)
 close(con_out)
+
