2dc54800173b4b7bf12e2cbf4d48bf2e4303913e
Jeroen Ooms <jeroenooms@gmail.com>
2014-09-11 22:40:44 +0200

    Add line to example

--- stuff/stream_io.R
+++ stuff/stream_io.R
@@ -1,8 +1,9 @@
 library(jsonlite)
+library(dplyr)
 con_in <- url("http://jeroenooms.github.io/data/diamonds.mjson")
 con_out <- file(tmp <- tempfile(), open = "w")
 stream_in(con_in, handler = function(df){
-  df <- filter(df, carat > 1)
+  df <- dplyr::filter(df, carat > 1)
   stream_out(df, con_out)
 })
 close(con_out)
