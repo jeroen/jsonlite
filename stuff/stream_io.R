ba96973ff1f8ee39238155088e4237604460f0c6
Jeroen Ooms <jeroenooms@gmail.com>
2014-09-11 22:38:23 +0200

    Add stream_io example

---
+++ stuff/stream_io.R
@@ -0,0 +1,8 @@
+library(jsonlite)
+con_in <- url("http://jeroenooms.github.io/data/diamonds.mjson")
+con_out <- file(tmp <- tempfile(), open = "w")
+stream_in(con_in, handler = function(df){
+  df <- filter(df, carat > 1)
+  stream_out(df, con_out)
+})
+close(con_out)
