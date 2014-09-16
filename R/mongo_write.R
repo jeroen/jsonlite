324dc9e3d8ffcee01dd2637c45c158fb23cfc4c4
Jeroen Ooms <jeroenooms@gmail.com>
2014-09-16 23:55:14 +0200

    Clean up

---
+++ R/mongo_write.R
@@ -0,0 +1,23 @@
+mongo_write <- function(x, mongo, ns, pagesize = 100, verbose = TRUE, ...){
+  # basic validation
+  stopifnot(is.data.frame(x))
+  stopifnot(is.character(ns))
+  stopifnot(rmongodb::mongo.is.connected(mongo))
+  apply_by_pages(x, mongo_write_page, pagesize = pagesize, verbose = verbose,
+    ns = ns, mongo = mongo, ...)
+}
+
+mongo_write_page <- function(x, ns, mongo, ...){
+  jsonlist <- parseJSON(asJSON(x, ...))
+  records <- lapply(jsonlist, mongo_create_record)
+  rmongodb::mongo.insert.batch(mongo, ns, records)
+}
+
+# This is a temporary hack because mongo.bson.from.list does not work for lists of length 0
+mongo_create_record <- function(x){
+  if(length(x)){
+    rmongodb::mongo.bson.from.list(x)
+  } else {
+    rmongodb::mongo.bson.empty()
+  }
+}
--- stuff/mongo.R
+++ stuff/mongo.R
@@ -1,5 +1,10 @@
-data1 <- fromJSON("https://api.github.com/users/hadley/repos")
-m <- mongo.create();
-jsonlite:::mongo_write(data1, m, "test.blahad2", pagesize=10, na="null")
-data2 <- jsonlite:::mongo_read(m, "test.blahad2")
+library(rmongodb)
+library(jsonlite)
 
+x1 <- fromJSON("https://api.github.com/users/hadley/repos")
+m <- mongo.create();
+table <- "test.mydata"
+mongo.remove(m, table)
+jsonlite:::mongo_write(x1, m, table, pagesize = 10, na="null")
+x2 <- jsonlite:::mongo_read(m, table, pagesize = 5)
+identical(x1, x2)
