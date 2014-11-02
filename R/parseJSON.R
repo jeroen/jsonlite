802b8c453f46ea4970a0df72ae33a508f2782285
Jeroen Ooms <jeroenooms@gmail.com>
2014-11-01 17:13:26 -0700

    Remove byte order mark if present.

--- R/parseJSON.R
+++ R/parseJSON.R
@@ -1,4 +1,8 @@
 #' @useDynLib jsonlite R_parse
 parseJSON <- function(txt) {
+  # Strip of byte order mark as suggested by rfc7159
+  if(substr(txt, 1, 1) == "\uFEFF"){
+    txt <- substring(txt, 2)
+  }
   .Call(R_parse, txt)
 }
