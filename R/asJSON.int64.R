113f4a8980582f87b3f00d204ff4bb096386c1d5
Jeroen Ooms <jeroenooms@gmail.com>
2014-11-06 15:43:48 -0800

    Fix for int64

--- R/asJSON.int64.R
+++ R/asJSON.int64.R
@@ -1,4 +1,9 @@
 setOldClass("int64")
-setMethod("asJSON", "int64", function(x, ...) {
+setMethod("asJSON", "int64", function(x, digits, ...) {
   asJSON(as.double(as.character(x)), digits = 0, ...)
+})
+
+setOldClass("integer64")
+setMethod("asJSON", "integer64", function(x, digits, ...) {
+  asJSON(as.double(x), digits = 0, ...)
 })
