3fac89eb22cfa5bc8899b62d3e0f06e3cd2ce584
Jeroen Ooms <jeroenooms@gmail.com>
2014-11-23 20:02:22 -0800

    Workaround for SSL3 problem.

--- R/download.R
+++ R/download.R
@@ -5,6 +5,18 @@ download_raw <- function(URL){
   download_retry_ssl <- function(e){
     res <- httr::GET(URL, httr::config(
       httpheader=c(`User-Agent` = "RCurl-httr-jsonlite", Accept="application/json, text/*, */*"),
+      ssl.verifypeer = FALSE,
+      ssl.verifyhost = FALSE
+    ));
+    #if this worked, show a warning. If not, it will just error.
+    warning(e$message, call. = FALSE)
+    return(res)
+  }
+
+  #retry with different SSL settings
+  download_retry_ssl3 <- function(e){
+    res <- httr::GET(URL, httr::config(
+      httpheader=c(`User-Agent` = "RCurl-httr-jsonlite", Accept="application/json, text/*, */*"),
       sslversion = 3,
       ssl.verifypeer = FALSE,
       ssl.verifyhost = FALSE
@@ -18,7 +30,7 @@ download_raw <- function(URL){
     httr::GET(URL, httr::config(
       httpheader=c(`User-Agent` = "RCurl-httr-jsonlite", Accept="application/json, text/*, */*")
     )),
-    SSL_CONNECT_ERROR = download_retry_ssl,
+    SSL_CONNECT_ERROR = download_retry_ssl3,
     SSL_PEER_CERTIFICATE = download_retry_ssl,
     SSL_CERTPROBLEM = download_retry_ssl,
     SSL_CACERT = download_retry_ssl
