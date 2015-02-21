9ba30c59921d0bbdd49c5be1ca9dd26012a0e21e
Jeroen Ooms <jeroenooms@gmail.com>
2015-02-20 20:57:33 -0800

    Comments

--- R/push_parser.R
+++ R/push_parser.R
@@ -1,11 +1,12 @@
-parse_con <- function(con, n = 1024){
+# Default to 100kb chunks.
+parse_con <- function(con, n = 100 * 1024){
   stopifnot(is(con, "connection"))
   if(!isOpen(con)){
     open(con, "rb")
     on.exit(close(con))
   }
   reset_push_parser();
-  while(length(buf <- readBin(con, raw(), 1024))){
+  while(length(buf <- readBin(con, raw(), n))){
     feed_push_parser(buf)
   }
   finalize_push_parser()
