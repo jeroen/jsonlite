5fddd6eb6fadfc24eaa3f577ba5cc8c468ceb1cd
Jonathan McPherson <jonathan@rstudio.com>
2015-03-05 11:28:20 -0800

    remove dependency on rstudioapi; pass R CMD check

--- R/init.R
+++ R/init.R
@@ -1,9 +1,11 @@
 .onAttach <- function(lib, pkg) {
   # remove View override in newer versions of RStudio, which can handle nested
-  # frames natively
-  if (rstudioapi::isAvailable("0.99.324")) {
-    unlockBinding("View", as.environment("package:jsonlite"))
-    assign("View", get("View", "package:utils"), "package:jsonlite")
-    lockBinding("View", as.environment("package:jsonlite"))
+  # frames natively; take no action if rstudioapi isn't present
+  rstudioapi <- "rstudioapi"
+  if (requireNamespace(rstudioapi, quietly = TRUE)) {
+    isAvailable <- get("isAvailable", envir = asNamespace(rstudioapi))
+    if (isAvailable("0.99.324")) {
+      assign("View", get("View", "package:utils"), "package:jsonlite")
+    }
   }
 }
