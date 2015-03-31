5174ca8151789487c36ee02ca11128b4a3f6c7f5
Jeroen Ooms <jeroenooms@gmail.com>
2015-03-30 18:46:00 -0700

    Fix for mongo

--- R/asJSON.raw.R
+++ R/asJSON.raw.R
@@ -5,7 +5,7 @@ setMethod("asJSON", "raw", function(x, raw = c("base64", "hex", "mongo"), ...) {
 
   # encode based on schema
   if (raw == "mongo") {
-    return(asJSON(list(`$binary` = as.scalar(base64_encode(x)), `$type` = as.scalar("\005"))))
+    return(asJSON(list(`$binary` = as.scalar(base64_encode(x)), `$type` = as.scalar("5"))))
   } else if (raw == "hex") {
     return(asJSON(as.character.hexmode(x), ...))
   } else {
