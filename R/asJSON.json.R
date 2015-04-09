86cef7c9dbc069778c9de2be38fe1352022aef71
Jeroen Ooms <jeroenooms@gmail.com>
2015-04-08 22:15:25 -0700

    Fix for asJSON.json warning

--- R/asJSON.json.R
+++ R/asJSON.json.R
@@ -1,4 +1,5 @@
 # If an object has already been encoded by toJSON(), do not encode it again
+setOldClass("json")
 setMethod("asJSON", "json", function(x, json_verbatim = FALSE, ...) {
   if(isTRUE(json_verbatim)){
     x
