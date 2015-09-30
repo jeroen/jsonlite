551f7af1a0741832e17e89ca1c8d45097c28a899
Jeroen Ooms <jeroenooms@gmail.com>
2015-09-30 14:06:28 +0200

    Drop int64 support.

--- R/asJSON.int64.R
+++ R/asJSON.int64.R
@@ -1,4 +1,4 @@
-setOldClass("int64")
-setMethod("asJSON", "int64", function(x, digits, ...) {
-  asJSON(as.double(as.character(x)), digits = 0, ...)
-})
+#setOldClass("int64")
+#setMethod("asJSON", "int64", function(x, digits, ...) {
+#  asJSON(as.double(as.character(x)), digits = 0, ...)
+#})
