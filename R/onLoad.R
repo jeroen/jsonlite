ada0dc681385a03245bd4ae1554260b59a76a6ea
Jeroen Ooms <jeroenooms@gmail.com>
2015-11-27 18:18:37 +0100

    Fix for CMD check in RStudio

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,7 +1,7 @@
 # Removes 'View' from jsonlite namespace in RStudio, which handles nested
 # frames natively.
 .onLoad <- function(lib, pkg) {
-  if (nchar(Sys.getenv("RSTUDIO"))) {
-    remove("View", pos = environment(.onLoad))
+  if (nchar(Sys.getenv("RSTUDIO")) && ("package:utils" %in% search())) {
+    try(assign("View", get("View", "package:utils"), pos = environment(.onLoad)))
   }
 }
