59212e6fbbca12c43f45a9df1201f568fef200dd
Jeroen Ooms <jeroenooms@gmail.com>
2016-05-08 16:29:47 +0200

    Optimization tweaks for arrays

--- R/collapse.R
+++ R/collapse.R
@@ -13,3 +13,22 @@ collapse <- function(x, inner = TRUE, indent = 0L) {
 row_collapse <- function(m, indent = NA_integer_){
   .Call(C_row_collapse_array, m, indent = indent)
 }
+
+
+# Iteratively collapse a high dimensional matrix / array
+# Does not perform the final collapse, which is done in asJSON.array()
+collapse_array <- function(x, columnmajor = FALSE, indent){
+  # dimensionality of the array
+  n <- length(dim(x))
+
+  # Collapse the inner vectors
+  dim <- 1:(n-1) + as.numeric(columnmajor)
+  x <- apply(x, dim, collapse, inner = TRUE, indent = indent)
+
+  # Collapse higher dimensions
+  for(i in rev(seq_along(dim(x)))[-1]) {
+    dim <- 1:(length(dim(x))-1) + as.numeric(columnmajor)
+    x <- apply(x, dim, collapse, inner = FALSE, indent = indent + 2L * i)
+  }
+  x
+}
