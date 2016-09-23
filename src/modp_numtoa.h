c0a9a572bc78a2307f5e5e30ba27eae60293077b
Jeroen Ooms <jeroenooms@gmail.com>
2016-09-23 21:55:48 +0200

    Update modp_numtoa from upsteam

--- src/modp_numtoa.h
+++ src/modp_numtoa.h
@@ -2,67 +2,64 @@
 /* vi: set expandtab shiftwidth=4 tabstop=4: */
 
 /**
- * \file
- *
- * <pre>
- * Copyright &copy; 2007, Nick Galbreath -- nickg [at] modp [dot] com
- * All rights reserved.
- * http://code.google.com/p/stringencoders/
- * Released under the bsd license.
- * </pre>
+ * \file modp_numtoa.h
+ * \brief Fast integer and floating-point numbers to string conversion
  *
  * This defines signed/unsigned integer, and 'double' to char buffer
  * converters.  The standard way of doing this is with "sprintf", however
  * these functions are
  *   * guarenteed maximum size output
  *   * 5-20x faster!
- *   * core-dump safe
+ *   * Won't core-dump
  *
+ * Note: this file is under and MIT license
+ */
+
+
+/**
+ * <pre>
+ * Copyright &copy; 2007, Nick Galbreath -- nickg [at] client9 [dot] com
+ * All rights reserved.
+ * http://code.google.com/p/stringencoders/
+ * Released under the MIT license.
+ * </pre>
  *
  */
 
 #ifndef COM_MODP_STRINGENCODERS_NUMTOA_H
 #define COM_MODP_STRINGENCODERS_NUMTOA_H
 
-#ifdef __cplusplus
-#define BEGIN_C extern "C" {
-#define END_C }
-#else
-#define BEGIN_C
-#define END_C
-#endif
+//#include "extern_c_begin.h"
 
-BEGIN_C
-
-#include <stdint.h>
+#include "modp_stdint.h"
 
 /** \brief convert an signed integer to char buffer
  *
  * \param[in] value
  * \param[out] buf the output buffer.  Should be 16 chars or more.
  */
-void modp_itoa10(int32_t value, char* buf);
+size_t modp_itoa10(int32_t value, char* buf);
 
 /** \brief convert an unsigned integer to char buffer
  *
  * \param[in] value
  * \param[out] buf The output buffer, should be 16 chars or more.
  */
-void modp_uitoa10(uint32_t value, char* buf);
+size_t modp_uitoa10(uint32_t value, char* buf);
 
 /** \brief convert an signed long integer to char buffer
  *
  * \param[in] value
  * \param[out] buf the output buffer.  Should be 24 chars or more.
  */
-void modp_litoa10(int64_t value, char* buf);
+size_t modp_litoa10(int64_t value, char* buf);
 
 /** \brief convert an unsigned long integer to char buffer
  *
  * \param[in] value
  * \param[out] buf The output buffer, should be 24 chars or more.
  */
-void modp_ulitoa10(uint64_t value, char* buf);
+size_t modp_ulitoa10(uint64_t value, char* buf);
 
 /** \brief convert a floating point number to char buffer with
  *         fixed-precision format
@@ -78,7 +75,7 @@ void modp_ulitoa10(uint64_t value, char* buf);
  * \param[in] precision  Number of digits to the right of the decimal point.
  *    Can only be 0-9.
  */
-void modp_dtoa(double value, char* buf, int precision);
+size_t modp_dtoa(double value, char* buf, int precision);
 
 /** \brief convert a floating point number to char buffer with a
  *         variable-precision format, and no trailing zeros
@@ -95,8 +92,14 @@ void modp_dtoa(double value, char* buf, int precision);
  * \param[in] precision  Number of digits to the right of the decimal point.
  *    Can only be 0-9.
  */
-void modp_dtoa2(double value, char* buf, int precision);
+size_t modp_dtoa2(double value, char* buf, int precision);
 
-END_C
+/**
+ * adds a 8-character hexadecimal representation of value
+ *
+ */
+char* modp_uitoa16(uint32_t value, char* buf, int final);
+
+//#include "extern_c_end.h"
 
 #endif
