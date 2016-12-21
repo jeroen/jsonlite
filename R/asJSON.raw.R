566ec747c708ac3326846bcc248c2d9bf325aebf
Jeroen Ooms <jeroenooms@gmail.com>
2016-12-21 13:40:25 +0100

    Tweak for mongo

--- R/asJSON.raw.R
+++ R/asJSON.raw.R
@@ -5,7 +5,8 @@ setMethod("asJSON", "raw", function(x, raw = c("base64", "hex", "mongo"), ...) {
 
   # encode based on schema
   if (raw == "mongo") {
-    return(asJSON(list(`$binary` = as.scalar(base64_enc(x)), `$type` = as.scalar("5"))))
+    type <- ifelse(length(attr(x, "type")), attr(x, "type"), 5)
+    return(asJSON(list(`$binary` = as.scalar(base64_enc(x)), `$type` = as.scalar(as.character(type)))))
   } else if (raw == "hex") {
     return(asJSON(as.character.hexmode(x), ...))
   } else {
