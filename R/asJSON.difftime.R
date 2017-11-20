37e2e2e7d79c8da54eed79d9fb1b5c4909da85d1
Jeroen <jeroenooms@gmail.com>
2017-11-21 00:35:11 +0100

    Add asJSON method for hms

---
+++ R/asJSON.difftime.R
@@ -0,0 +1,10 @@
+setOldClass(c("hms", "difftime"))
+setMethod("asJSON", "hms", function(x, hms = c("string", "secs"), ...) {
+  hms <- match.arg(hms)
+  output <- switch(hms,
+                   string = as.character(x),
+                   secs = as.numeric(x, units = "secs")
+  )
+  output[is.na(x)] <- NA
+  asJSON(output, ...)
+})
