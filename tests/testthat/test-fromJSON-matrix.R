66b48dee3197c81d648a5d8faea3551bc4480031
Jeroen Ooms <jeroenooms@gmail.com>
2019-12-27 14:09:08 +0100

    Workaround for round() changes in R 4.0 The new implementation of round() in R no longer matches printf() rounding.

--- tests/testthat/test-fromJSON-matrix.R
+++ tests/testthat/test-fromJSON-matrix.R
@@ -10,16 +10,16 @@ test_that("fromJSON Matrix", {
     matrix(round(pi,2)),
     matrix(c(1,NA,2,NA), 2),
     volcano,
-    matrix(NA)    
+    matrix(NA)
   );
-  
+
   lapply(objects, function(object){
     newobject <- fromJSON(toJSON(object));
     expect_that(newobject, is_a("matrix"));
     expect_that(object, equals(newobject));
   });
-  
-  expect_that(fromJSON(toJSON(objects)), equals(objects));  
+
+  expect_that(fromJSON(toJSON(objects)), equals(objects));
 });
 
 test_that("fromJSON Matrix with simplifyMatrix=FALSE", {
@@ -32,13 +32,16 @@ test_that("fromJSON Matrix with simplifyMatrix=FALSE", {
 
 test_that("fromJSON Matrix datasets", {
   objects <- Filter(is.matrix, lapply(ls("package:datasets"), get));
-  
+
   lapply(objects, function(object){
     class(object) <- "matrix";
     newobject <- fromJSON(toJSON(object, digits=4))
     expect_that(newobject, is_a("matrix"));
     expect_that(dim(newobject), equals(dim(object)));
     attributes(newobject) <- attributes(object);
-    expect_that(newobject, equals(round(object,4)));
+
+    # R has changed rounding algo in 4.0 and no longer matches printf
+    #expect_that(newobject, equals(round(object,4)))
+    expect_equal(newobject, object, tolerance = 1e-4)
   });
 });
