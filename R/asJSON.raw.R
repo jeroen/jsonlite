881226cc026925fc3fbe29d601679d55d2c7d64f
Jeroen Ooms <jeroenooms@gmail.com>
2020-10-16 14:15:43 +0200

    Add blob method (#334)

--- R/asJSON.raw.R
+++ R/asJSON.raw.R
@@ -1,3 +1,11 @@
+setMethod("asJSON", "blob", function(x, raw = "base64", ...){
+  if(identical(raw, "base64")){
+    str <- vapply(x, base64_enc, character(1))
+    return(asJSON(str, ...))
+  }
+  asJSON(as.list(x), raw = raw, ...)
+})
+
 setMethod("asJSON", "raw", function(x, raw = c("base64", "hex", "mongo", "int", "js"), ...) {
 
   # validate
