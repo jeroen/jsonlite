6a30acaead6539f11de19f4d02d1866d04fa5d43
Hong Ooi <hongooi73@gmail.com>
2021-03-17 11:29:56 +1100

    Use vctrs::vec_rbind to bind paged results (#352)
    * use vctrs:vec_rbind for binding pages
    
    * redocument
    
    * update vignette

--- R/rbind_pages.R
+++ R/rbind_pages.R
@@ -7,10 +7,15 @@
 #' book. In practice this is often implemented using a \code{page} parameter in the API. The
 #' \code{rbind_pages} function can be used to combine these pages back into a single dataset.
 #'
-#' The \code{\link{rbind_pages}} function generalizes \code{\link[base:cbind]{base::rbind}} and
-#' \code{\link[plyr:rbind.fill]{plyr::rbind.fill}} with added support for nested data frames. Not each column
-#' has to be present in each of the individual data frames; missing columns will be filled
-#' up in \code{NA} values.
+#' The \code{rbind_pages} function uses \code{\link[vctrs:vec_rbind]{vctrs::vec_rbind}}
+#' to bind the pages together. This generalizes \code{\link[base:cbind]{base::rbind}} in two
+#' ways:
+#'
+#' \itemize{
+#'   \item Not each column has to be present in each of the individual data frames; missing
+#'   columns will be filled up in \code{NA} values.
+#'   \item Data frames can be nested (can contain other data frames).
+#' }
 #'
 #' @export
 #' @param pages a list of data frames, each representing a \emph{page} of data
@@ -32,56 +37,14 @@
 #' colnames(organizations)
 #' }
 rbind_pages <- function(pages){
-  #Load plyr
-  loadpkg("plyr")
+  loadpkg("vctrs")
 
   #validate input
   stopifnot(is.list(pages))
-
-  # edge case
-  if(!length(pages)){
-    return(data.frame())
-  }
 
   # All elements must be data frames or NULL.
   pages <- Filter(function(x) {!is.null(x)}, pages);
   stopifnot(all(vapply(pages, is.data.frame, logical(1))))
 
-  # Extract data frame column names
-  dfdf <- lapply(pages, vapply, is.data.frame, logical(1))
-  dfnames <- unique(names(which(unlist(unname(dfdf)))))
-
-  # No sub data frames
-  if(!length(dfnames)){
-    return(plyr::rbind.fill(pages))
-  }
-
-  # Extract the nested data frames
-  subpages <- lapply(dfnames, function(colname){
-    rbind_pages(lapply(pages, function(df) {
-      if(!is.null(df[[colname]]))
-        df[[colname]]
-      else
-        as.data.frame(matrix(nrow=nrow(df), ncol=0))
-    }))
-  })
-
-  # Remove data frame columns
-  pages <- lapply(pages, function(df){
-    issubdf <- vapply(df, is.data.frame, logical(1))
-    if(any(issubdf))
-      df[issubdf] <- rep(NA, nrow(df))
-    df
-  })
-
-  # Bind rows
-  outdf <- plyr::rbind.fill(pages)
-
-  # Combine wih sub dataframes
-  for(i in seq_along(subpages)){
-    outdf[[dfnames[i]]] <- subpages[[i]]
-  }
-
-  #out
-  outdf
+  do.call(vctrs::vec_rbind, pages)
 }
