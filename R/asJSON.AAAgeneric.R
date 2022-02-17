2d20549a641428361c1461a1396685607d2476ff
Kirill Müller <krlmlr+r@mailbox.org>
2022-02-17 21:27:33 +0100

    Add compatibility setOldClass() call for blob

--- R/asJSON.AAAgeneric.R
+++ R/asJSON.AAAgeneric.R
@@ -4,12 +4,13 @@ setGeneric("asJSON", function(x, ...) {
 
 if(getRversion() < 4){
   setOldClass("AsIs")
+  setOldClass(c("blob", "vctrs_list_of", "vctrs_vctr"))
   setOldClass("integer64")
   setOldClass(c("hms", "difftime"))
-  setOldClass('ITime')
+  setOldClass("ITime")
   setOldClass("json")
   setOldClass("pairlist")
   setOldClass("scalar")
-  setOldClass('sf')
-  setOldClass('sfc')
+  setOldClass("sf")
+  setOldClass("sfc")
 }
