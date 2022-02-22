ae617d2673eba7d6b2826327df6677c0709fc6dc
Jeroen Ooms <jeroenooms@gmail.com>
2022-02-22 16:01:23 +0100

    Fix docs a bit

--- R/json_gzip.R
+++ R/json_gzip.R
@@ -4,6 +4,12 @@
 #' network space. This is mainly effective for larger json objects with many
 #' repeated keys, as is common in serialized data frames.
 #'
+#' The [as_gzjson_raw] and [parse_gzjson_raw] functions work with raw (binary)
+#' vectors of compressed data. To use this in a place where only text is allowed
+#' you can wrap the output again in [base64] as done by [as_gzjson_b64] and
+#' [parse_gzjson_b64]. This increases the size again with about 33%.
+#'
+#'
 #' @param x R data object to be converted to JSON
 #' @param ... passed down to [toJSON] or [fromJSON]
 #' @export
@@ -19,6 +25,13 @@ as_gzjson_raw <- function(x, ...){
 
 #' @export
 #' @rdname gzjson
+as_gzjson_b64 <- function(x, ...){
+  buf <- as_gzjson_raw(x = x, ...)
+  base64_enc(buf)
+}
+
+#' @export
+#' @rdname gzjson
 #' @param buf  raw vector with gzip compressed data
 #' @param simplifyVector passed to [fromJSON]
 parse_gzjson_raw <- function(buf, simplifyVector = TRUE, ...){
@@ -28,14 +41,7 @@ parse_gzjson_raw <- function(buf, simplifyVector = TRUE, ...){
 
 #' @export
 #' @rdname gzjson
-as_gzjson_b64 <- function(x, ...){
-  buf <- as_gzjson_raw(x = x, ...)
-  base64_enc(buf)
-}
-
-#' @export
-#' @rdname gzjson
 #' @param b64 base64 encoded string containing gzipped json data
-parse_gzjson_b64 <- function(b64){
-  parse_gzjson_raw(base64_dec(b64))
+parse_gzjson_b64 <- function(b64, ...){
+  parse_gzjson_raw(base64_dec(b64), ...)
 }
