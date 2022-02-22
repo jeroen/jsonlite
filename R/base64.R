60b9a6fa02a947f712962bfc65ed61cd44aefb7d
Jeroen Ooms <jeroenooms@gmail.com>
2022-02-22 15:52:46 +0100

    Add base64url wrappers

--- R/base64.R
+++ R/base64.R
@@ -4,6 +4,10 @@
 #' raw vectors to text. Interchangeable with encoder from `base64enc` or
 #' `openssl` package.
 #'
+#' The [base64url_enc] and [base64url_dec] functions use a variation of base64
+#' that substitute characters `+/`  for `-_` respectively, such that the output
+#' does not require URL-encoding. See also section 5 of rfc4648.
+#'
 #' @param input string or raw vector to be encoded/decoded
 #' @export
 #' @rdname base64
@@ -31,4 +35,29 @@ base64_enc <- function(input) {
   }
   stopifnot(is.raw(input))
   .Call(R_base64_encode, input)
+}
+
+#' @export
+#' @rdname base64
+base64url_enc <- function(bin){
+  text <- base64_enc(bin)
+  sub("=+$", "", chartr('+/', '-_', text))
+}
+
+#' @export
+#' @rdname base64
+base64url_dec <- function(text){
+  text <- fix_padding(chartr('-_', '+/', text))
+  base64_dec(text)
+}
+
+# Ensures base64 length is a multiple of 4
+fix_padding <- function(text){
+  text <- gsub("[\r\n]", "", text)[[1]]
+  mod <- nchar(text) %% 4;
+  if(mod > 0){
+    padding <- paste(rep("=", (4 - mod)), collapse = "")
+    text <- paste0(text, padding)
+  }
+  text
 }
