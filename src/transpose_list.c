02e340923e815543bdfd1f5da42e05fd47917849
Henrik Lindberg <henrik@hnlk.se>
2022-07-14 21:29:12 +0200

    Improve performance of transpose_list()
    This improves the runtime significantly for loading data with many
    columns. The order of loop nesting as well as a much more efficient
    binary search does the trick.
    
    In a real world example, fetching ~300k rows with ~50 columns from
    MongoDB, this brings the query + load time from 70 seconds to ~40.
    
    Microbenchmark with synthetic data on an AMD 5950X, 128GB RAM, Fedora
    Linux 36, R 4.1.3, jsonlite 1.8.0.9000 commit 80854359
    
    ```
    > set.seed(1)
    > rows <- 10000
    > columns <- 100
    > p_missing <- 0.2
    >
    > recordlist <- lapply(1:rows, function(rownum) {
    +   row <- as.list(1:columns)
    +   names(row) <- paste0("col_", row)
    +   row[runif(columns) > p_missing]
    + })
    > columns <- unique(unlist(lapply(recordlist, names), recursive = FALSE,
    +                          use.names = FALSE))
    ```
    
    Before this change
    
    ```
    > microbenchmark::microbenchmark(
    +     jsonlite:::transpose_list(recordlist, columns),
    +     times = 10
    + )
    Unit: milliseconds
                                               expr      min       lq     mean   median       uq      max neval
     jsonlite:::transpose_list(recordlist, columns) 577.8338 589.4064 593.0518 591.6895 599.4221 607.3057    10
    ```
    
    With this change
    
    ```
    > microbenchmark::microbenchmark(
    +     jsonlite:::transpose_list(recordlist, columns),
    +     times = 10
    + )
    Unit: milliseconds
                                               expr      min       lq     mean   median       uq      max neval
     jsonlite:::transpose_list(recordlist, columns) 41.37537 43.22655 43.88987 43.76705 45.43552 46.81052    10
    ```

--- src/transpose_list.c
+++ src/transpose_list.c
@@ -1,26 +1,55 @@
 #include <Rinternals.h>
 #include <string.h>
 
+// names is assumed to be sorted, to make names matching faster
+// by using a binary search
 SEXP C_transpose_list(SEXP x, SEXP names) {
   size_t ncol = Rf_length(names);
   size_t nrow = Rf_length(x);
   SEXP out = PROTECT(allocVector(VECSXP, ncol));
+
+  // Allocate output
   for(size_t i = 0; i < ncol; i++){
-    const char * targetname = CHAR(STRING_ELT(names, i));
     SEXP col = PROTECT(allocVector(VECSXP, nrow));
-    for(size_t j = 0; j < nrow; j++){
-      //search for 'targetname' in each record j
-      SEXP list = VECTOR_ELT(x, j);
-      SEXP listnames = getAttrib(list, R_NamesSymbol);
-      for(size_t k = 0; k < Rf_length(listnames); k++){
-        if(!strcmp(CHAR(STRING_ELT(listnames, k)), targetname)){
+    SET_VECTOR_ELT(out, i, col);
+    UNPROTECT(1);
+  }
+
+  // Find and save all elements in their transposed place
+  for(size_t j = 0; j < nrow; j++){
+    SEXP list = VECTOR_ELT(x, j);
+    SEXP listnames = getAttrib(list, R_NamesSymbol);
+    size_t listlength = Rf_length(listnames);
+
+    for(size_t k = 0; k < listlength; k++){
+      const char * listname = CHAR(STRING_ELT(listnames, k));
+
+      // Binary search for a name match
+      size_t low = 0;
+      size_t high = ncol - 1;
+      size_t mid;
+      while(1){
+        mid = (low + high) / 2;
+        const char * targetname = CHAR(STRING_ELT(names, mid));
+
+        int strcmp_result = strcmp(listname, targetname);
+        if(strcmp_result == 0){
+          // Match!
+          SEXP col = VECTOR_ELT(out, mid);
           SET_VECTOR_ELT(col, j, VECTOR_ELT(list, k));
+          break;
+        } else if (strcmp_result > 0){
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+
+        if (low > high) {
+          // No match to be found
           break;
         }
       }
     }
-    SET_VECTOR_ELT(out, i, col);
-    UNPROTECT(1);
   }
   //setAttrib(out, R_NamesSymbol, names);
   UNPROTECT(1);
