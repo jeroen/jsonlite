467923567ce2c2d535714cc3908a6912000a29ae
Jeroen Ooms <jeroenooms@gmail.com>
2023-02-24 23:26:20 +0100

    Cleanups

--- tidy.R
+++
@@ -1,9 +0,0 @@
-#script to clean up code
-library(formatR)
-
-#see ?tidy.source
-options(reindent.spaces=2)
-options(replace.assign=TRUE)
-lapply(list.files("R", full.name=TRUE), function(x){
-  try(tidy.source(x, file=x, width.cutoff=80))
-})
