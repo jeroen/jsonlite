ed48568237425979f8e22a93089798ddff8c7883
Murat Tasan <mmuurr@gmail.com>
2024-01-09 16:38:47 +0100

    safely attempt S3 dispatch

--- R/asJSON.ANY.R
+++ R/asJSON.ANY.R
@@ -6,19 +6,34 @@ setMethod("asJSON", "ANY", function(x, force = FALSE, ...) {
     } else {
       stop("No method for S4 class:", class(x))
     }
-  } else if (length(class(x)) > 1) {
-    # If an object has multiple classes, we recursively try the next class. This is
-    # S3 style dispatching that doesn't work by default for formal method definitions
-    # There should be a more native way to accomplish this
-    return(asJSON(structure(x, class = class(x)[-1]), force = force, ...))
-  } else if (isTRUE(force) && existsMethod("asJSON", class(unclass(x))[1])) {
-    # As a last resort we can force encoding using the unclassed object
-    return(asJSON(unclass(x), force = force, ...))
-  } else if (isTRUE(force)) {
-    return(asJSON(NULL))
-    warning("No method asJSON S3 class: ", class(x))
-  } else {
-    # If even that doesn't work, we give up.
-    stop("No method asJSON S3 class: ", class(x))
   }
+  
+  ## walk the s3 class chain (if any):
+  if (length(class(x)) > 1) {
+    for (cls in class(x)[-1]) {
+      f <- getMethod("asJSON", cls, optional = TRUE)
+      if (!is.null(f)) return(f(x, ...))
+    }
+  }
+  
+  ## fallback class:
+  if (isTRUE(force)) {
+    f <-
+      tryCatch({
+        getMethod("asJSON", class(unclass(x))[1], optional = TRUE)  ## unclass() can err, hence tryCatch()
+      }, error = function(e) NULL)
+    if (!is.null(f)) return(f(x, ...))
+  }
+
+  msg <- sprintf("No method asJSON S3 class: %s", paste0(class(x), collapse = ","))
+  
+  ## final fallback -- encode NULL and warn:
+  if (isTRUE(force)) {
+    warning(msg)
+    return(asJSON(NULL))
+  }
+  
+  ## give up.
+  stop(msg)
 })
+
