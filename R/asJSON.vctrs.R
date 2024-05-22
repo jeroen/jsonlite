b44da5c5dc3526a1b276de5e906baa7f6da2e094
Eric Coissac <eric@coissac.eu>
2024-05-22 20:34:20 +0200

    Patch the infinite recursion of asJSON on subclasses of vctrs_vctr instances.

--- R/asJSON.vctrs.R
+++ R/asJSON.vctrs.R
@@ -1,5 +1,21 @@
 setMethod("asJSON", "vctrs_vctr", function(x, ...) {
-  # dispatch based on the underlying type
-  class(x) <- setdiff(class(x), 'vctrs_vctr')
-  asJSON(x, ...)
+  tryCatch(
+    {
+      asJSON(vec_proxy_json(x), ...)
+    },
+    error = function(cond) {
+      selectMethod(asJSON, "ANY")(x)
+    }
+  )
 })
+
+#' @export
+vec_proxy_json <- function(x) {
+  UseMethod("vec_proxy_json", x)
+}
+
+#' @export
+vec_proxy_json.vctrs_vctr <- function(x) {
+  class(x) <- setdiff(class(x), "vctrs_vctr")
+  x
+}
