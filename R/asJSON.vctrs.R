022e01fb22d65bbc4e08bcea58aa608d6dd84590
Eric Coissac <eric@coissac.eu>
2024-05-22 21:21:30 +0200

    Add documentation for the generic S3 method vec_proxy_json

--- R/asJSON.vctrs.R
+++ R/asJSON.vctrs.R
@@ -9,6 +9,16 @@ setMethod("asJSON", "vctrs_vctr", function(x, ...) {
   )
 })
 
+#' JSON conversion proxy
+#' 
+#' vec_proxy_json method returns proxy objects, i.e. an atomic vector or a list of atomic vectors,
+#' able to be converted to JSON using the jsonlite package. This is a generic S3 method, that has
+#' to be implemented for subclasses requesting particualar JSON conversion. A default implementation
+#' is provided for `vctrs_vctr` class.
+#' 
+#' @param x object instance of class `vctrs_vctr` to be converted to JSON
+#' 
+#' @return an atomic vector or a list of atomic vectors.
 #' @export
 vec_proxy_json <- function(x) {
   UseMethod("vec_proxy_json", x)
