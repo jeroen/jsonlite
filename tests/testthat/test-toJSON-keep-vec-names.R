a112d976ec002cec047a013cd26c7ba72ae03ebd
Garrick Aden-Buie <garrick@adenbuie.com>
2025-10-14 10:04:36 -0400

    tests(keep_vec_names): Messages aren't expected anymore

--- tests/testthat/test-toJSON-keep-vec-names.R
+++ tests/testthat/test-toJSON-keep-vec-names.R
@@ -3,20 +3,20 @@ context("toJSON keep_vec_names")
 test_that("keep_vec_names with named vectors", {
   # Basic types should give messages
   # Length-1 vectors
-  expect_message(expect_equal(toJSON2(c(a = 1)), '{"a":1}'))
-  expect_message(expect_equal(toJSON2(c(a = "x")), '{"a":"x"}'))
-  expect_message(expect_equal(toJSON2(c(a = TRUE)), '{"a":true}'))
+  expect_equal(toJSON2(c(a = 1)), '{"a":1}')
+  expect_equal(toJSON2(c(a = "x")), '{"a":"x"}')
+  expect_equal(toJSON2(c(a = TRUE)), '{"a":true}')
 
   # Longer vectors
-  expect_message(expect_equal(toJSON2(c(a = 1, b = 2)), '{"a":1,"b":2}'))
-  expect_message(expect_equal(toJSON2(c(a = "x", b = "y")), '{"a":"x","b":"y"}'))
-  expect_message(expect_equal(toJSON2(c(a = FALSE, b = TRUE)), '{"a":false,"b":true}'))
+  expect_equal(toJSON2(c(a = 1, b = 2)), '{"a":1,"b":2}')
+  expect_equal(toJSON2(c(a = "x", b = "y")), '{"a":"x","b":"y"}')
+  expect_equal(toJSON2(c(a = FALSE, b = TRUE)), '{"a":false,"b":true}')
 
   # Some other types
-  expect_message(expect_equal(toJSON2(factor(c(a = "x"))), '{"a":"x"}'))
-  expect_message(expect_equal(toJSON2(c(a = as.Date("2015-01-01"))), '{"a":"2015-01-01"}'))
-  expect_message(expect_equal(toJSON2(c(a = as.POSIXct("2015-01-01 3:00:00"))), '{"a":"2015-01-01 03:00:00"}'))
-  expect_message(expect_equal(toJSON2(c(a = as.POSIXlt("2015-01-01 3:00:00"))), '{"a":"2015-01-01 03:00:00"}'))
+  expect_equal(toJSON2(factor(c(a = "x"))), '{"a":"x"}')
+  expect_equal(toJSON2(c(a = as.Date("2015-01-01"))), '{"a":"2015-01-01"}')
+  expect_equal(toJSON2(c(a = as.POSIXct("2015-01-01 3:00:00"))), '{"a":"2015-01-01 03:00:00"}')
+  expect_equal(toJSON2(c(a = as.POSIXlt("2015-01-01 3:00:00"))), '{"a":"2015-01-01 03:00:00"}')
 
   # keep_vec_names shouldn't affect unnamed vectors
   expect_equal(toJSON2(1), '1')
